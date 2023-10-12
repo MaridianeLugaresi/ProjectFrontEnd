58f2087478e282e52671d2397e1b4db72dfd882a
Maridiane Lugaresi <maridiane.lugaresi@hotmail.com>
2023-10-12 16:05:14 -0300

    Adicionada a codificacao da pagina de login

--- js/script.js
+++ js/script.js
@@ -1,5 +1,29 @@
-//efeito do botão voltar ao Topo
+function voltarTopo(){
+    window.scrollTo(
+        {
+            top:0,
+            left:0,
+            behavior:'smooth'
+        }
+    )
+}
 
-//Validação de Login
+function login(){
+    var logado = 0;
+    var usuario = document.getElementById("usuario").value;
+    var senha = document.getElementById("senha").value;
+    
+    if(usuario == 'admin' && senha == '123456'){
+        window.location = "index.html";
+        logado = 1;
+    }
 
-//Ativar alert no botão cadastrar
+    if(logado == 0){
+        alert("Acesso negado!")
+    }
+}
+
+function cadastro(){
+    alert("Cadastrado com sucesso");
+    window.location.href = "index.html";
+}
